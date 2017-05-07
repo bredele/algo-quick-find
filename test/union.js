88364df2fc8ff3c33ba2bf285dbba8ecbc4c0d1a
bredele <olivier.wietrich@gmail.com>
2017-05-07 15:59:08 -0600

    should union one node with an other one that belongs already to a component

--- test/union.js
+++ test/union.js
@@ -11,3 +11,12 @@ test('should union two nodes given their ids', assert => {
   union(ids, 2, 3)
   assert.deepEqual(ids, [0, 1, 3, 3, 4, 5])
 })
+
+
+test('should union one node with an other one that belong to a component', assert => {
+  assert.plan(1)
+  const ids = [0, 1, 2, 3, 4, 5]
+  union(ids, 2, 3)
+  union(ids, 3, 5)
+  assert.deepEqual(ids, [0, 1, 5, 5, 4, 5])
+})
