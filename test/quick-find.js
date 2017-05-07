30b7afd9a681eb7dc443fc80bf5d3b0461e3225e
bredele <olivier.wietrich@gmail.com>
2017-05-07 16:54:23 -0600

    Use automatically generated list of ids

--- test/quick-find.js
+++ test/quick-find.js
@@ -2,6 +2,7 @@
  * Tests dependencies.
  */
 
+const array = require('array-ids')
 const test = require('tape')
 const union = require('../union')
 const find = require('../find')
@@ -9,14 +10,14 @@ const algo = require('..')
 
 test('should union two nodes given their ids', assert => {
   assert.plan(1)
-  const ids = [0, 1, 2, 3, 4, 5]
+  const ids = array(6)
   union(ids, 2, 3)
   assert.deepEqual(ids, [0, 1, 3, 3, 4, 5])
 })
 
 test('should union one node with an other one that belong to a component', assert => {
   assert.plan(1)
-  const ids = [0, 1, 2, 3, 4, 5]
+  const ids = array(6)
   union(ids, 2, 3)
   union(ids, 3, 5)
   assert.deepEqual(ids, [0, 1, 5, 5, 4, 5])
@@ -24,7 +25,7 @@ test('should union one node with an other one that belong to a component', asser
 
 test('should find two connect nodes', assert => {
   assert.plan(2)
-  const ids = [0, 1, 2, 3, 4, 5]
+  const ids = array(6)
   assert.equal(find(ids, 3, 5), false)
   union(ids, 3, 5)
   assert.equal(find(ids, 3, 5), true)
@@ -32,7 +33,7 @@ test('should find two connect nodes', assert => {
 
 test('test quick find api', assert => {
   assert.plan(2)
-  const quickFind = algo([0, 1, 2, 3])
+  const quickFind = algo(array(4))
   assert.equal(quickFind.connected(1, 2), false)
   quickFind.union(1, 2)
   assert.equal(quickFind.connected(1, 2), true)
