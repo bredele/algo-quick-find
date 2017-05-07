01bb2642124538503e09a0dc8ed342dd99f5f9f8
bredele <olivier.wietrich@gmail.com>
2017-05-07 16:09:56 -0600

    add documentation

--- find.js
+++ find.js
@@ -1,4 +1,13 @@
 
+/**
+ * Find two connected nodes using the quick find algorithm.
+ *
+ * @param {Array} ids
+ * @param {Number} p
+ * @param {Number} q
+ * @return {Boolean} true if connected
+ * @api public
+ */
 
 module.exports = function(ids, p, q) {
   return ids[p] === ids[q]
